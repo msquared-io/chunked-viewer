cff4b8b62610c02aea38ca16e7197937ca15645d
MontagueM <montaguemoran+github@improbable.io>
2025-05-09 15:39:43 +0100

    change to use spaceconfig

--- src/etherbaseConfig.ts
+++ src/etherbaseConfig.ts
@@ -6,13 +6,9 @@ const localUrl = "http://localhost"
 export const etherbaseConfig: EtherbaseConfig = {
   httpReaderUrl: useLocalBackend
     ? `${localUrl}:8082`
-    : "https://etherbase-reader-496683047294.europe-west2.run.app",
-  wsReaderUrl: useLocalBackend
-    ? `${localUrl}:8082`
-    : "wss://etherbase-reader-496683047294.europe-west2.run.app",
-  wsWriterUrl: useLocalBackend
-    ? `${localUrl}:8081`
-    : "wss://etherbase-writer-496683047294.europe-west2.run.app",
+    : "https://spaceconfig.com",
+  wsReaderUrl: useLocalBackend ? `${localUrl}:8082` : "wss://spaceconfig.com",
+  wsWriterUrl: useLocalBackend ? `${localUrl}:8081` : "wss://spaceconfig.com",
   // privateKey:
   // "0x0c9a44c9e7778f9f3132ab2ad581b1473f84683e6b42da3938160dc602ee29d0",
   useBackend: true,
