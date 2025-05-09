e0e737562dfa8a84427ee546c43c4896e6681c76
MontagueM <montaguemoran+github@improbable.io>
2025-05-09 15:42:28 +0100

    Revert

--- src/etherbaseConfig.ts
+++ src/etherbaseConfig.ts
@@ -6,9 +6,13 @@ const localUrl = "http://localhost"
 export const etherbaseConfig: EtherbaseConfig = {
   httpReaderUrl: useLocalBackend
     ? `${localUrl}:8082`
-    : "https://spaceconfig.com",
-  wsReaderUrl: useLocalBackend ? `${localUrl}:8082` : "wss://spaceconfig.com",
-  wsWriterUrl: useLocalBackend ? `${localUrl}:8081` : "wss://spaceconfig.com",
+    : "https://etherbase-reader-496683047294.europe-west2.run.app",
+  wsReaderUrl: useLocalBackend
+    ? `${localUrl}:8082`
+    : "wss://etherbase-reader-496683047294.europe-west2.run.app",
+  wsWriterUrl: useLocalBackend
+    ? `${localUrl}:8081`
+    : "wss://etherbase-writer-496683047294.europe-west2.run.app",
   // privateKey:
   // "0x0c9a44c9e7778f9f3132ab2ad581b1473f84683e6b42da3938160dc602ee29d0",
   useBackend: true,
